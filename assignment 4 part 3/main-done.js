11ad019e4994bdf2165798e9c7672d4a1409ef28
calebsaliendra <157448058+calebsaliendra@users.noreply.github.com>
2024-04-09 10:52:55 -0400

    Update main-done.js

--- assignment 4 part 3/main-done.js
+++ assignment 4 part 3/main-done.js
@@ -1,4 +1,3 @@
-// set up canvas
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -6,13 +5,11 @@ const ctx = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-// function to generate random number
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random RGB color value
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
@@ -76,8 +73,7 @@ const balls = [];
 while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the edge of the canvas, to avoid drawing errors
+ 
     random(0 + size, width - size),
     random(0 + size, height - size),
     random(-7, 7),
